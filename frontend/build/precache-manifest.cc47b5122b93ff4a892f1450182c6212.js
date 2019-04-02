f084c5b26c36849c1529c582e2ebcd86790a819a
Jennifer Enck <jenniferenck@mac.com>
2019-04-02 10:50:09 -0700

    updated build to deploy to heroku

---
+++ frontend/build/precache-manifest.cc47b5122b93ff4a892f1450182c6212.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "94f9a5593f2bbeb9ae0cf2f279205329",
+    "url": "/static/media/genericLogo.94f9a559.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0a00e91ef5d6cb55e81",
+    "url": "/static/js/main.b0a00e91.chunk.js"
+  },
+  {
+    "revision": "45e04e51f9fd60593683",
+    "url": "/static/js/1.45e04e51.chunk.js"
+  },
+  {
+    "revision": "b0a00e91ef5d6cb55e81",
+    "url": "/static/css/main.8b2f7c02.chunk.css"
+  },
+  {
+    "revision": "45e04e51f9fd60593683",
+    "url": "/static/css/1.8440b225.chunk.css"
+  },
+  {
+    "revision": "a343757451ce67742ae049f5850a3d3c",
+    "url": "/index.html"
+  }
+];
